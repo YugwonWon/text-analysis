2d27b5471554539ada0ced2b41b1cfdc6b445ef9
원유권 <yugwon@baikal.ai>
2024-04-24 20:19:03 +0900

    토픽 모델링 추가
    - 토픽 모델링

--- src/train/topic_modeling.py
+++ src/train/topic_modeling.py
@@ -2,6 +2,7 @@ import json
 import glob
 import csv
 import os
+import pandas as pd
 from tqdm import tqdm
 
 from bareunpy import Tagger
@@ -13,6 +14,10 @@ from google.protobuf.json_format import MessageToDict
 
 # 형태소 분석을 통해 명사만 추출하는 함수
 def extract_nouns(text):
+    """
+    형태소 분석을 통해 명사만을 추출합니다.
+    :param text(str): 분석할 텍스트
+    """
     nouns = []
     analysis_results = tagger.tag(text)
     for sentence in analysis_results.sentences:
@@ -21,22 +26,33 @@ def extract_nouns(text):
                 nouns.append(token.text)
     return nouns
 
+def load_data(path):
+    """
+    폴더에서 json 목록을 구해 원하는 텍스트를 추출합니다.
+    :param path: json이 들어 있는 파일 경로
+    """
+    print('> 데이터 로딩')
+    json_list = glob.glob(os.path.join(path, '**/*.json'), recursive=True)
+    # 뉴스 데이터 불러오기
+    news_data = []
+    for filename in json_list:
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for article in data["data"]:
+                code = article["doc_class"]["code"] # 카테고리
+                for paragraph in article["paragraphs"]:
+                    context = paragraph["context"] # 뉴스기사 본문
+                    news_data.append((code, context))
+    print('> 데이터 로딩 완료\n')
+    return news_data
+
 
 class TopicModeling:
-    stopwords = set(['들', '그', '저'])
+    stopwords = set(['들']) # 불용어가 필요한 경우 여기에 추가합니다.
     
     def __init__(self, documents, tagger):
         self.documents = documents
         self.tagger = tagger
-        
-    def make_join_sentence(self, documents):
-        len_lines = len(documents)
-        num_iters = len_lines // 2 + (1 if len_lines % 2 != 0 else 0)
-        join_texts = []
-        for i in range(num_iters):
-            joined_text = " ".join(documents[2 * i: min(2 * (i + 1), len_lines)])
-            join_texts.append(joined_text)
-        return join_texts
 
     def extract_nouns(self, text):
         try:
@@ -49,89 +65,86 @@ class TopicModeling:
             for token in sentence["tokens"]:
                 for morph in token["morphemes"]:
                     if morph["tag"].startswith('NNG') and \
-                        morph["text"]["content"] not in TopicModeling.stopwords:  # 명사를 확인하는 조건
+                        morph["text"]["content"] not in TopicModeling.stopwords:  # 명사를 확인하는 조건: 태그가 명사이고, 불용어 리스트에 없는 경우
                         nouns.append(morph["text"]["content"])  # 명사만을 리스트에 추가
         return nouns
 
     def preprocess_documents(self):
-        processed_docs_by_code = {}
-        for code, docs in tqdm(self.documents.items(), desc='documents'):
-            # join_texts = self.make_join_sentence(docs)
-            processed_documents = []
-            for text in tqdm(docs, desc='category'):
-                nouns = self.extract_nouns(text)
-                if len(nouns)==0:
-                    print('Error AnalyzeText')
-                    continue
-                processed_documents.extend(nouns)
-            processed_docs_by_code[code] = processed_documents
-        return processed_docs_by_code
+        print('> 전처리 실행(형태소 분석, 명사추출)')
+        processed_documents = []
+        for _, text in tqdm(self.documents, desc='documents'):
+            nouns = self.extract_nouns(text)
+            if len(nouns)==0:
+                print('Error AnalyzeText')
+                continue
+            processed_documents.append(nouns)
+        print('> 전처리 완료\n')
+        print('> 전처리된 뉴스 데이터 샘플')
+        for doc in processed_documents[:5]:  # 처음 5개 문서만 출력
+          print(' '.join(doc))
+        return processed_documents
 
-    def run_lda(self, processed_docs_by_code):
-        lda_models_by_code = {}
-        for code, docs in processed_docs_by_code.items():
-            dictionary = corpora.Dictionary([docs])
-            corpus = [dictionary.doc2bow(doc) for doc in [docs]]
-            lda_model = LdaModel(corpus=corpus, num_topics=10, id2word=dictionary, passes=15)
-            lda_models_by_code[code] = [lda_model, corpus]
-        return lda_models_by_code
+    def run_lda_on_combined_corpus(self, processed_docs):
+        print('\n> LDA 학습 시작')
+        dictionary = corpora.Dictionary(processed_docs)
+        corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
+        lda_model = LdaModel(corpus=corpus, num_topics=10, id2word=dictionary, passes=15)
+        print('> LDA 학습 종료')
+        return lda_model, corpus, dictionary
 
-    def run(self, num_topics=11, num_words=5, output_dir='out/csv'):
-        code_dict = {}
-        processed_docs_by_code = self.preprocess_documents()
-        lda_models_by_code = self.run_lda(processed_docs_by_code)
-        # 각 분야별 토픽 출력
-        
+    def run(self, num_topics=10, num_words=5, output_dir='out/csv'):
+        print('\n> 토픽 모델링 시작')
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
+
+        # 전처리
+        processed_docs = self.preprocess_documents()
+
+        #LDA 학습
+        lda_model, corpus, dictionary = self.run_lda_on_combined_corpus(processed_docs)
+
+        # LDA 결과 저장
         with open(f'{output_dir}/LDA분석_결과.csv', mode='w', encoding='utf-8', newline='') as file:
             writer = csv.writer(file)
-            # Write the header row
-            writer.writerow(['Code', 'Topic Index', 'Topic'])
-            for code, [lda_model, corpus] in lda_models_by_code.items():
-                print(f"Code: {code}")
-                dictionary = lda_model.id2word  # 수정된 run_lda 메서드로부터 반환받은 값
-                for idx, topic in lda_model.show_topics(formatted=True, num_topics=num_topics, num_words=num_words):
-                    print(f"Topic {idx}: {topic}")
-                    writer.writerow([code, f"Topic {idx}", topic])
-                print("\n")
-                code_dict[code] = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
-        return code_dict
+            writer.writerow(['Topic Index', 'Topic'])
+            for idx, topic in lda_model.show_topics(formatted=True, num_topics=num_topics, num_words=num_words):
+                writer.writerow([f"Topic {idx}", topic])
 
-if __name__ == '__main__':    
+        # LDAvis 준비 및 반환
+        vis_data = pyLDAvis.gensim_models.prepare(lda_model, corpus, dictionary)
+        print('> LDAvis 생성')
+        return vis_data
+
+if __name__ == '__main__':
     # 바른 형태소 분석기 초기화
-    tagger = Tagger("koba-6BVW24Q-2MIEHVQ-WQAVXJI-E2TRS7A", 'localhost', 5656)
-    
-    # 뉴스 데이터 불러오기 및 전처리
-    news_data = []
-    for filename in glob.glob('/home/yugwon/text-analysis/data/news/01.데이터/1.Training/**/*.json', recursive=True):
-        with open(filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            for article in data["data"]:
-                code = article["doc_class"]["code"]
-                for paragraph in article["paragraphs"]:
-                    context = paragraph["context"]
-                    news_data.append((code, context))
-                    
-    # code 별로 문장을 분류
-    news_by_code = {}
-    for code, context in news_data:
-        if code not in news_by_code:
-            news_by_code[code] = []
-        news_by_code[code].append(context)
-    
-    # _news_by_code = {}
-    # for code, context in news_by_code.items():
-    #     _news_by_code[code] = context[:5]
-    # news_by_code = _news_by_code
-        
-    tm = TopicModeling(documents=news_by_code, tagger=tagger)
-    code_dict = tm.run(num_topics=5, num_words=5)
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    tagger = Tagger(config['bareun_api_key'], 'localhost', 5757)
+
+    news_data = load_data(path="data/news")
+    news_df = pd.DataFrame(news_data)
+    print('뉴스 데이터 샘플')
+    print(news_df.head(5))
+
+    # 테스트를 위해 500개만 선택(전체 샘플은 약 16만 문장)
+    news_data = news_data[:500]
+
+    # 토픽 모델링을 시작합니다.
+    tm = TopicModeling(documents=news_data, tagger=tagger)
+    vis_data = tm.run(num_topics=10, num_words=5)
+
+    # 디렉토리가 없다면 만듭니다.
+    if not os.path.exists('out/pkl'):
+        os.makedirs('out/pkl')
+
     import pickle
-    with open('code_dict.pkl', 'wb') as file:
-        pickle.dump(code_dict, file)
-    pyLDAvis.enable_notebook()
-    # 시각화
-    pyLDAvis.display(code_dict['정치'])
+    # 저장할 파일명 수정
+    save_name = 'out/pkl/vis_data2.pkl'
+    with open(save_name, 'wb') as file:
+        pickle.dump(vis_data, file)
+    print(f'> LDAvis data 저장 완료 -> {save_name}')
+    # pyLDAvis.enable_notebook()
+    # # 시각화
+    # pyLDAvis.display(vis_data)
     
     
